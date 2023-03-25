a956859793973105f8d5f9366afffc128b8d7e34
maxijayme <maxijayme@gmail.com>
2023-03-25 16:27:26 +0100

    testing: direct push

---
+++ main/main.js
@@ -0,0 +1,16 @@
+
+document.querySelector('#post1 i').addEventListener("click", addOrRemovelike);
+document.querySelector('#post2 i').addEventListener("click", addOrRemovelike);
+document.querySelector('#post3 i').addEventListener("click", addOrRemovelike);
+
+function addOrRemovelike(){
+    if(this.classList.contains('bi-heart')){
+        this.classList.remove('bi-heart')
+        this.classList.add('bi-heart-fill');
+        this.classList.add('like');
+    }else{
+        this.classList.add('bi-heart');
+        this.classList.remove('bi-heart-fill')
+        this.classList.remove('like')
+    }
+}
