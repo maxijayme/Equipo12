429c0c6f0267771a4c50926a2c25e0e77fd13536
Enedina1977 <gmenendezenedina@gmail.com>
2023-04-27 20:12:52 +0200

    modified:   api/package.json 	modified:   client/app.js     modified:   client/index.html 	modified:   client/styles/styles.css

--- client/app.js
+++ client/app.js
@@ -50,3 +50,13 @@ function enableButton(){
         registerButton.disabled = true;
     }
 }
+
+// Script Google
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var id_token = googleUser.getAuthResponse().id_token;
+  }
