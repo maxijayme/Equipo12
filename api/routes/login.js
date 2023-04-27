34f5d909135f5f89f1651771079a5557a0da583d
maxijayme <maxijayme@gmail.com>
2023-04-27 10:14:42 +0200

    consulta a tusuario ok

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const initialState = require('../../client/initialState.js');
 const validateLogin = require('../middlewares/validate.js');
-const db = require('../db.js');
+const db = require('../db/db.js');
+const { QueryTypes } = require('sequelize');
 
 router.post('/', validateLogin, async(req,res)=>{
     try{
         const {user,password}=req.body;
-        const loginUser =  await db.query(`Select * from tusuario where username = "${user}" and password = "${password}" `)
+        const loginUser =  await db.query(`Select * from tusuario where username = "${user}" and password = "${password}" `, { type: QueryTypes.SELECT })
         console.log(loginUser);
         if(loginUser.length>0){
             res.status(200).json({msj:'usuario logueado exitosamente'})
