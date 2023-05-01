f70d4e8e17cb2030d2a91c5e1359defc5406d068
Sonia <soniaglezsuarez@gmail.com>
2023-05-01 21:45:36 +0200

    prueba servidor

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,19 +1,19 @@
-const { Router } = require('express');
-const router = Router();
-const db = require('../db/db.js');
-const { QueryTypes } = require('sequelize');
+// const { Router } = require('express');
+// const router = Router();
+// const db = require('../db/db.js');
+// const { QueryTypes } = require('sequelize');
 
 
-router.get('/:id', async (req,res)=>{
-    console.log(id);
-    try{
-        const {id} = req.params;
-        const actualUser = await db.query(`Select phone,email,linkedin,photo from tusuario where username = "${id}"`, { type: QueryTypes.SELECT });
-        console.log(actualUser);
-        res.status(200).json(actualUser);
-    }catch(err){
-        console.send(err);
-    }
-});
+// router.get('/:id', async (req,res)=>{
+//     console.log(id);
+//     try{
+//         const {id} = req.params;
+//         const actualUser = await db.query(`Select phone,email,linkedin,photo from tusuario where username = "${id}"`, { type: QueryTypes.SELECT });
+//         console.log(actualUser);
+//         res.status(200).json(actualUser);
+//     }catch(err){
+//         console.send(err);
+//     }
+// });
 
-module.exports=router;
+// module.exports=router;
