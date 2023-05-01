43ca4e27148e010594fc11210029138279e8aab9
Sonia <soniaglezsuarez@gmail.com>
2023-05-02 00:12:25 +0200

    cargar datos hoja personal

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,19 +1,22 @@
-// const { Router } = require('express');
-// const router = Router();
-// const db = require('../db/db.js');
-// const { QueryTypes } = require('sequelize');
+const { Router } = require('express');
+const router = Router();
+const db = require('../db/db.js');
+const { QueryTypes } = require('sequelize');
 
 
-// router.get('/:id', async (req,res)=>{
-//     console.log(id);
-//     try{
-//         const {id} = req.params;
-//         const actualUser = await db.query(`Select phone,email,linkedin,photo from tusuario where username = "${id}"`, { type: QueryTypes.SELECT });
-//         console.log(actualUser);
-//         res.status(200).json(actualUser);
-//     }catch(err){
-//         console.send(err);
-//     }
-// });
+router.get('/:id', async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const contactUser = await db.query(`Select phone,email,linkedin,photo from tusuario where id_usuario = "${id}"`, { type: QueryTypes.SELECT });
+        const jobsUser = await db.query(`Select puesto,empresa,funciones,f_inicio,f_fin,actualidad from ttrabajos where id_usuario = "${id}"`, { type: QueryTypes.SELECT });
+        const studiesUser = await db.query(`Select titulo,centro,f_inicio,f_fin,actualidad from testudios where id_usuario = "${id}"`, { type: QueryTypes.SELECT });
+        const otherDataUser = await db.query(`Select licencia,disponibilidad,preferencia,hobbies from totros_datos where id_usuario = "${id}"`, { type: QueryTypes.SELECT });
+        const actualUser = [contactUser,jobsUser,studiesUser,otherDataUser];
+        console.log(actualUser);
+        res.status(200).json(actualUser);
+    }catch(err){
+        console.log(err);
+    }
+});
 
-// module.exports=router;
+module.exports=router;
--- client/hojaPersonal/hojaPersonal.js
+++ client/hojaPersonal/hojaPersonal.js
@@ -1,18 +1,13 @@
-const userList = JSON.parse(localStorage.getItem("userList"));
-const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-
-const exist = userList.filter(user => user.id == currentUser.id)
-
 const userPhoto = document.getElementById('profilePhoto');
 const userPhone = document.getElementById('phone');
 const userEmail = document.getElementById('email');
 const userLinkedin = document.getElementById('linkedin')
+const jobsContainer = document.getElementById('jobs');
+const studiesContainer = document.getElementById('studies');
+const otherDataContainer = document.getElementById('otherData');
+const photoNavBar = document.getElementById('photoNavBar');
 
 const actualUser = localStorage.getItem("userIdTeclapedia");
-
-if(exist.length == 0){
-    location.href=document.referrer
-}
 
 let bDelete = document.getElementById("deleteAccount");
 bDelete.addEventListener('click', deleteAccount)
@@ -52,10 +47,74 @@ function loader2(){
 
 // llamada para coger los datos
 async function getUserData(){
-    await fetch('http://localhost:3001/profile/'+actualUser)
+    await fetch('http://localhost:3001/profile/'+"3")
     .then(data=> data.json())
     .then(data=>{
-        console.log(data);
+        console.log(data)
+        
+        // personal data
+        const contact = data[0]
+        photoNavBar.setAttribute("src",contact[0].photo);
+        userPhoto.setAttribute("src",contact[0].photo);
+        contact[0].phone && (userPhone.innerHTML=contact[0].phone);
+        contact[0].email && (userEmail.innerHTML=contact[0].email);
+        contact[0].linkedin && (userLinkedin.innerHTML=contact[0].linkedin);
+        // experience
+        const jobs = data[1];
+        jobs.forEach(job => {
+            const newJob = document.createElement('div');
+            let endJob = (job.actualidad ? "actualidad" : job.f_fin);
+            newJob.innerHTML=`
+                <div class="row m-2 d-flex">
+                    <h4>${job.puesto}</h4>
+                    <h5>${job.empresa}</h5>
+                    <h6>${job.f_inicio} --- ${endJob}</h6>
+                    <p>${job.funciones}</p>
+                </div>`;
+            jobsContainer.appendChild(newJob);
+        });
+
+        //degrees
+        const degrees = data[2];
+        degrees.forEach(degree=>{
+            const newDegree = document.createElement('div');
+            let endDegree = (degree.actualidad ? "actualidad" : degree.f_fin);
+            newDegree.innerHTML=`
+                <div class="row m-2 d-flex">
+                    <h4>${degree.titulo}</h4>
+                    <div class="row">
+                        <h5 class="col">${degree.centro}</h5>
+                        <h6 class="col">${degree.f_inicio} - ${endDegree}</h6>
+                    </div>
+                </div>`;
+            studiesContainer.appendChild(newDegree);
+        });
+
+        // other data
+        const otherData = data[3];
+        const newData = document.createElement('div');
+        let available = (otherData[0].disponibilidad ? "Sí" : "No");
+        let licence = (otherData[0].licencia ? otherData[0].licencia:"No");
+        console.log(otherData);
+        newData.innerHTML= `<div class="row m-2 d-flex">
+                                <div class="col">
+                                    <h5>Hobbies</h5>
+                                    <p>${otherData[0].hobbies}</p>
+                                </div>
+                                <div class="col">
+                                    <h5>Carnet</h5>
+                                    <p>${licence}</p>
+                                </div>
+                                <div class="col">
+                                    <h5>Disponibilidad Inmediata</h5>
+                                    <p>${available}</p>
+                                 </div>
+                                 <div class="col">
+                                    <h5>Preferencia</h5>
+                                    <p>${otherData[0].preferencia}</p>
+                                </div>
+                            </div>`;
+        otherDataContainer.appendChild(newData)
     })
     .catch(console.error)
 }
