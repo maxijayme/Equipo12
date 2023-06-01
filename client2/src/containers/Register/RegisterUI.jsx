62153acbfa5c0eaf83ccc36ea7d93311ab1eba5f
Sonia <soniaglezsuarez@gmail.com>
2023-06-01 12:07:56 +0200

    validado register y dados estilos

--- client2/src/containers/Register/RegisterUI.jsx
+++ client2/src/containers/Register/RegisterUI.jsx
@@ -2,80 +2,119 @@ import React from "react";
 import { Field, reduxForm } from 'redux-form';
 import './Register.css';
 
+// Validation
+const validate = values => {
+    const errors = {}
+    if(!values.fullname) {
+        errors.fullname = 'Campo obligatorio'
+    }
+    if (!values.userName) {
+      errors.userName = 'Campo obligatorio'
+    } else if (values.userName.length < 4) {
+      errors.userName = 'El nombre de usuario debe tener al menos 4 caracteres'
+    }
+    if (!values.email) {
+      errors.email = 'Campo obligatorio'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Formato de email inválido'
+    }
+    if (!values.password) {
+      errors.password = 'Campo obligatorio'
+    } else if (values.password.length < 8) {
+      errors.password = 'La contraseña debe tener al menos 8 caracteres'
+    } else if (values.password.length > 20) {
+      errors.password = 'La contraseña debe tener menos de 20 caracteres'
+    }
+
+    if (!values.passwordConfirm) {
+        errors.passwordConfirm = 'Campo obligatorio'
+    } else if (values.password !== values.passwordConfirm) {
+        errors.passwordConfirm = 'Las contraseñas no coinciden'
+    }
+    return errors
+  }
+
+const renderField = ({
+    className,
+    input,
+    label,
+    type,
+    meta: { touched, error }
+    }) => (
+    <div>
+        <div>
+            <input {...input} placeholder={label} type={type} className={className}/>
+            {touched && (error && <span>{error}</span>)}
+        </div>
+    </div>
+)
+
 function RegisterUI({handleSubmit, pristine, submitting}){
     return(
         <>
-            <div className="col-6" id="container-initial">
-                <h1>¡Regístrate y comparte información con otros junior developers!</h1>
-            </div>
-            <div className="col-6" id="container-initialr">
-                <div className="col-6 bg-body-tertiary bg-dark">
-                    <img src="./img/teclapedia_logo.svg" id="logo" alt=""/>
+            <div className="completeRegister">
+                <div className="col-6" id="container-initial">
+                    <h1 id="titleRegister">¡Regístrate y comparte información con otros junior developers!</h1>
                 </div>
-                <div id="container-form">
-                    <form className="row" id="data_form" onSubmit={handleSubmit}>
-                        <Field
-                            className="input_form form-control mb-2"
-                            placeholder="Nombre y Apellidos"
-                            type="text"
-                            name="fullname"
-                            component="input"
-                        />
-                        <Field
-                            className="input_form form-control mb-2"
-                            placeholder="Nombre de Usuario"
-                            type="text"
-                            name="userName"
-                            component="input"
-                        />
-                        <label className="form-label align-self-center me-2" htmlFor="userName"></label>
-                        <Field
-                            className="input_form form-control mb-2"
-                            placeholder="Email"
-                            type="text"
-                            name="email"
-                            component="input"
-                        />
-                        <label className="form-label align-self-center me-2" htmlFor="email"></label>
-                        <Field
-                            className="input_form form-control mb-2"
-                            placeholder="Contraseña"
-                            type="password"
-                            name="password"
-                            component="input"
-                        />
-                        <label className="form-label align-self-center me-2" htmlFor="password"></label>
-                        <Field
-                            className="input_form form-control mb-2"
-                            placeholder="Confirmación contraseña"
-                            type="password"
-                            name="passwordConfirm"
-                            component="input"
-                        />
-                        <label className="form-label align-self-center me-2" htmlFor="password"></label>
-                        <p className="p-cond">Al registrarte, aceptas nuestras Condiciones. Obtén más información sobre cómo recopilamos, usamos y compartimos tus datos en la Política de privacidad, así como el uso que hacemos de las cookies y tecnologías similares en la Política de cookies.</p>
-                        <button id="register" type="submit" disabled={pristine|submitting}>Registrarse</button>
-                    </form>
-                    <div className="row" id="division">
-                        <div className="line"></div>
-                        <span>o</span>
-                        <div className="line"></div>
+                <div className="col-6" id="container-initialr">
+                    <div className="col-6 bg-body-tertiary bg-dark">
+                        <img src="./img/teclapedia_logo.svg" id="logo-register" alt=""/>
                     </div>
-                    
+                    <div id="container-form-register">
+                        <form className="row" id="data_form_register" onSubmit={handleSubmit}>
+                            <Field
+                                className="input_form_register form-control mb-2"
+                                label="Nombre y Apellidos"
+                                type="text"
+                                name="fullname"
+                                component={renderField}
+                            />
+                            <Field
+                                className="input_form_register form-control mb-2"
+                                label="Nombre de Usuario"
+                                type="text"
+                                name="userName"
+                                component={renderField}
+                            />
+                            <Field
+                                className="input_form_register form-control mb-2"
+                                label="Email"
+                                type="text"
+                                name="email"
+                                component={renderField}
+                            />
+                            <Field
+                                className="input_form_register form-control mb-2"
+                                label="Contraseña"
+                                type="password"
+                                name="password"
+                                component={renderField}
+                            />
+                            <Field
+                                className="input_form_register form-control mb-2"
+                                label="Confirmación contraseña"
+                                type="password"
+                                name="passwordConfirm"
+                                component={renderField}
+                            />
+                            <p className="p-cond">Al registrarte, aceptas nuestras Condiciones. Obtén más información sobre cómo recopilamos, usamos y compartimos tus datos en la Política de privacidad, así como el uso que hacemos de las cookies y tecnologías similares en la Política de cookies.</p>
+                            <button className="button_register" id="register_button" type="submit" disabled={pristine|submitting}>Registrarse</button>
+                        </form>
+                    </div>
+                    <div className="row" id="allReadyRegistered">
+                        <p>¿Ya tienes cuenta?</p>
+                        <a href="../index.html">Inicia</a>
+                    </div>
                 </div>
-                <div className="row" id="login">
-                    <p>¿Ya tienes cuenta?</p>
-                    <a href="../index.html">Inicia</a>
-                </div>
-            </div> 
-            <div id="loader">            
-                <span>Usuario registrado con éxito</span>
-            </div> 
+            </div>           
         </>
     )
 }
 
-const createReduxForm = reduxForm({form:'firstRegister'});
+const createReduxForm = reduxForm({
+    form:'firstRegister',
+    validate
+});
 
 RegisterUI = createReduxForm(RegisterUI);
 
--- client2/src/containers/Register/index.js
+++ client2/src/containers/Register/index.js
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout"
 import RegisterUI from "./RegisterUI"
 import {URL} from '../../utils/url' 
 
+
 export default function Register(){
     
+    const navigate = useNavigate();
+    const [error, setError] = useState('')
+
     const handleSubmit = async(values) => {
         console.log(values)
-
+        
         // para comprobar si existe
         try {
             const response = await fetch(`${URL}/register/exist`, {
@@ -20,40 +26,42 @@ export default function Register(){
                 const responseJson = await response.json();
                 console.log(responseJson);
                 if(responseJson.dataEmail){
+                    setError("El email ya está en uso")
                     console.log('El email ya está en uso')
-                    //emailLabel.innerHTML = 'El email ya está en uso';
-                    //exist = false;
                 }
                 if(responseJson.dataUserName){
-                    console.log('El usuario ya está en uso')
-                    //userNameLabel.innerHTML = 'El usuario ya está en uso';
-                    //exist = false;
+                    setError('El usuario ya está en uso')
+                    console.log('El usuario ya está en uso')                    
                 } 
             } else {
-                console.log('todo ok')
+                //Este para crear después el nuevo usuario
+                await fetch(`${URL}/register`,{
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(values)
+                    }).then(data => {
+                        if(data.status === 200){
+                            console.log('Usuario registrado')
+                            navigate('/form')
+                        } else if (data.status === 400){                            
+                            console.log('Usuario registrado')
+                            setError('Usuario registrado')
+                        }
+                })
             }
         } catch (error) {
             console.error("Error:", error);
-        }
-
-        // Este para crear después el nuevo usuario
-        // await fetch(`${URL}/register`,{
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify(values)
-        //     }).then(data => {
-        //         if(data.status === 200){
-        //             console.log(data)
-        //         } else if (data.status === 400){
-        //             console.log('Error')
-        //         }
-        // })
+            setError(error.message || 'Hubo un error en el registro')
+        }        
     }
     
     return(
         <Layout navbarOff={false}>
+            <div className={error ? "error-message" : "hidden"}>
+                {error && <p>{error}</p>}
+            </div>
             <RegisterUI onSubmit={handleSubmit}/>
         </Layout>
     )
