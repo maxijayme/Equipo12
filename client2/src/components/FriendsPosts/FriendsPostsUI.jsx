5bdf227a5903e5da279a5fa68e743a858ca43d55
maxijayme <maxijayme@gmail.com>
2023-06-11 08:43:02 +0200

    fix: height html, body, root

--- client2/src/components/FriendsPosts/FriendsPostsUI.jsx
+++ client2/src/components/FriendsPosts/FriendsPostsUI.jsx
@@ -3,7 +3,7 @@ import './FriendsPosts.css'
 
 export default function FriendsPostsUI({friendsPostList}) {
     return (
-        <div id="posts_container">
+        <div id="posts_container" className="container">
           {friendsPostList.length > 0 && friendsPostList.map((post,i)=>(
             <PostCard key={i} {...post}/>
           ))  }
